c011363b8477781481d25a1be8c4b40fe058463c
Varush Siva <106482656+VarushSiva@users.noreply.github.com>
2023-07-06 20:06:08 -0400

    fetch movie data from TMDB api and log into console

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from './axios';
 
-function Row({title}) {
-  return (
-    <div>
-        <h2>{title}</h2>
-    </div>
+function Row({title, fetchUrl}) {
+    const  [movies, setMovies] = useState([]);
+
+    // Snippet of code which runs based on specific conditions
+    useEffect(() => {
+        // [], run only once when row loads
+        async function fetchData() {
+            const requests = await axios.get(fetchUrl);
+            console.log(requests);
+            return requests;
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <h2>{title}</h2>
+
+            {/* container > movie posters */}
+        </div>
   )
 };
   
