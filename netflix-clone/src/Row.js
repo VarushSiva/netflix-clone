4f11f5c36286d9316b431a000dc774f19e5792e3
Varush Siva <106482656+VarushSiva@users.noreply.github.com>
2023-07-06 21:02:00 -0400

    Link movie posters

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import axios from './axios';
 
+const base_url = "https://image.tmdb.org/t/p/original";
+
 function Row({title, fetchUrl}) {
     const  [movies, setMovies] = useState([]);
 
@@ -9,15 +11,23 @@ function Row({title, fetchUrl}) {
         // [], run only once when row loads
         async function fetchData() {
             const requests = await axios.get(fetchUrl);
-            console.log(requests);
+            setMovies(requests.data.results);
             return requests;
         }
         fetchData();
-    }, []);
+    }, [fetchUrl]);
 
     return (
-        <div>
+        <div className='row'>
             <h2>{title}</h2>
+
+            <div className='row-poster'>
+                {/* row-posters */}
+
+                {movies.map(movie => (
+                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+                ))}
+            </div>
 
             {/* container > movie posters */}
         </div>
