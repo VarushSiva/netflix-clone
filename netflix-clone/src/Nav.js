8f2c0a874a4a16bbad97a520d6d4a787558b3d4e
Varush Siva <106482656+VarushSiva@users.noreply.github.com>
2023-07-07 00:15:53 -0400

    Add Nav CSS + transition when scrolling

--- netflix-clone/src/Nav.js
+++ netflix-clone/src/Nav.js
@@ -1,9 +1,22 @@
-import React from 'react'
-import './Nav.css'
+import React, { useEffect, useState } from 'react';
+import './Nav.css';
 
 function Nav() {
+    // Nav affect to show black background after scrollY exceeds 100
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else handleShow(false);
+        });
+        return () => {
+            window.removeEventListener("scroll", null);
+        };
+    }, []);
   return (
-    <div className='nav'>
+    <div className={`nav ${show && "nav-black"}`}>
         <img 
             className='nav-logo'
             src='https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg'
